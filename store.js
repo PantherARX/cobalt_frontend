18b06601712cd9a27196c9e1b9b97dad9ac7ca3e
Santiago Giraldo <sgv2604@gmail.com>
2024-04-02 19:11:23 -0500

    store module finished

--- store.js
+++ store.js
@@ -2,6 +2,13 @@ const decrease = document.querySelector("#decrease_quantity");
 const quantity = document.querySelector("#product_quantity");
 const increase = document.querySelector("#increase_quantity");
 
+const shoppingCart = document.querySelector("#shoppingCart");
+const checkout = document.querySelector("#checkoutContainer");
+
+shoppingCart.addEventListener("click", () => {
+    checkout.classList.toggle("hidden")
+})
+
 console.log(typeof Number(quantity.innerText ));
 
 decrease.addEventListener("click", () => {
