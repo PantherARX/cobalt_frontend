0d0d7c7f7284325a6206ca384e0aa1d4f10a641b
Santiago Giraldo <sgv2604@gmail.com>
2024-04-03 02:39:50 -0500

    fixed rerservas v2

---
+++ reserves.js
@@ -0,0 +1,20 @@
+const reserves = document.querySelectorAll(".property_img");
+const buttonReserve = document.querySelector(".button-input");
+
+reserves.forEach((e) => {
+    e.addEventListener("click", () => {
+        const test = e.querySelector(".reserves")
+        test.classList.toggle("hidden");
+        const button = e.querySelector(".button-input");
+        // button.addEventListener("click", () => {
+        //     console.log("click");
+        //     test.classList.toggle("hidden");
+        // });
+    });
+});
+
+buttonReserve.addEventListener("click", () => {
+    const reserve = document.querySelector(".property_img");
+    reserve.classList.toggle("hiddden");
+    
+});
