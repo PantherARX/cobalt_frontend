cefe431c1ebab55b9792a18015cd656b59d86528
Santiago Giraldo <sgv2604@gmail.com>
2024-04-03 20:45:33 -0500

    fixed notif cards

--- reserves.js
+++ reserves.js
@@ -2,14 +2,21 @@ const reserves = document.querySelectorAll(".property_img");
 const buttonReserve = document.querySelector(".button-input");
 
 reserves.forEach((e) => {
+    
     e.addEventListener("click", () => {
         const test = e.querySelector(".reserves")
         test.classList.toggle("hidden");
+        e.parentElement.parentElement.setAttribute("disabled", "");
+        console.log(e.innerHTML);
+        // e.setAttribute("disabled", "");
+        
     });
+
 });
 
 buttonReserve.addEventListener("click", () => {
     const reserve = document.querySelector(".property_img");
+    console.log("click")
     reserve.classList.toggle("hiddden");
     
 });
